65ec82d126efd0344787019b347251ad908f14dc
TheRoccoB <rocco.balsamo@gliffy.com>
2018-09-07 13:29:57 -0700

    upload image demo

---
+++ Assets/Scripts/ImportImage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UI;
+
+public class ImportImage : MonoBehaviour {
+
+	public string url = "https://docs.unity3d.com/uploads/Main/ShadowIntro.png";
+
+	IEnumerator Start()
+	{
+		//start download
+		var www = new WWW(url);
+
+		//wait until download completes
+		yield return www; // wait for www to return
+
+		//create a DXT1 (compressed) texture
+		Texture2D texture = new Texture2D(www.texture.width, www.texture.height, TextureFormat.DXT1, false);
+		
+		www.LoadImageIntoTexture(texture);
+		Rect rect = new Rect(0, 0, texture.width, texture.height);
+		Sprite sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f), 100);
+
+		GetComponent<Image>().sprite = sprite;
+
+
+	}	
+	
+}
